5e77b35f9cad802934b8613d676dbbd6a908946d
Arthur Santos Gatti <arthurgatti@unifesspa.edu.br>
2026-05-13 18:00:21 -0300

    Erro de divisão

---
+++ src/ArithmeticException/Divisao.java
@@ -0,0 +1,17 @@
+package ArithmeticException;
+
+public class Divisao {
+    private double num1;
+    private double num2;
+    private double div;
+    public Divisao(double num1, double num2) {
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+    public void dividir (){
+        this.div = this.num1 / this.num2;
+    }
+    public void exibirDivisao(){
+        System.out.println("O resultado da divisão é: "+this.div);
+    }
+}
