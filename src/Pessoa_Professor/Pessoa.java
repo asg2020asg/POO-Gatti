c11990e6389a6baa8bb869c772f97f7c19fbbbb6
Arthur Santos Gatti <arthurgatti@unifesspa.edu.br>
2026-05-14 12:06:56 -0300

    Atividade 8

---
+++ src/Pessoa_Professor/Pessoa.java
@@ -0,0 +1,14 @@
+package Pessoa_Professor;
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+    public void apresentar(){
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+    }
+}
