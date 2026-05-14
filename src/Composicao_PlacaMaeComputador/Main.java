e2251ef14d92e338ad9b54e25531c75044d27db4
Arthur Santos Gatti <arthurgatti@unifesspa.edu.br>
2026-05-14 11:10:58 -0300

    atividade 7

---
+++ src/Composicao_PlacaMaeComputador/Main.java
@@ -0,0 +1,13 @@
+package Composicao_PlacaMaeComputador;
+
+public class Main {
+    public static void main(String[] args) {
+        Computador c1= new Computador("Dell","Inspiron 15","Intel Core i5",8,"Asus","B560",4,"DDR4");
+        c1.exibirComputadorCompleto();
+        System.out.println("atualização em andamento...");
+        c1.atualizarProcessador("Intel Core i7");
+        c1.expandirMemoria(8);
+        c1.atualizarPlacaMae("Z590","DDR5");
+        c1.exibirComputadorCompleto();
+    }
+}
